bf4fc0573f8b44ca0c2f2ea58a0880d0c45ad3c8
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-06 11:03:28 -0400

    mutations & auth

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers
@@ -20,18 +19,16 @@ app.use(express.json());
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
-
-db.once('open', () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      // log where we can go to test our GQL API
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-};
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
